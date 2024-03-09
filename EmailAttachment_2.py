1fbe6c11d4f33af85b3bfa4119a2cba22484c0e9
adityasahane07 <94785176+adityasahane07@users.noreply.github.com>
2024-03-09 15:20:38 +0530

    Add files via upload

---
+++ EmailAttachment_2.py
@@ -0,0 +1,41 @@
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+
+email = 'infinityokayy@gmail.com'
+password = "xrit vgiv lyth ocvk"
+send_to_email = 'adityamsahane07@gmail.com'
+subject = 'Automated Email'
+message = 'with attachment'
+
+file_location = 'C:/APL/Python and machine learning/class/Automation/26_nov/abc.txt'
+
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = send_to_email
+msg['Subject'] = subject
+
+msg.attach(MIMEText(message, 'plain'))
+
+# Setup the attachment
+filename = os.path.basename(file_location)
+attachment = open(file_location, "rb")
+part = MIMEBase('application', 'octet-stream')
+part.set_payload(attachment.read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+# Attach the attachment to the MIMEMultipart object
+msg.attach(part)
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email, password)
+text = msg.as_string()
+server.sendmail(email, send_to_email, text)
+server.quit()
